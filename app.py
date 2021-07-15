c3b60de8c0b908f89587e1873b3590cf24984348
IndorA <IndorA@gitlab.com>
2021-07-15 20:26:56 +0700

    Last

--- app.py
+++ app.py
@@ -176,6 +176,64 @@ def sentiment_result(value):
 # fwk = frequent_word_key[:5]
 # fwv = frequent_word_value[:5]
 
+
+# if df.head() =='TextReviewData':
+#     print("tang ina mo")
+# else:
+#     print("dogshit")
+
+# soleID = "1uEmRODg6j1KZr0yVNTfWz2Y_pamTHWO1zDEaW1rRrK8"
+    # sole = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{soleID}/export?format=csv')
+    # #
+    # # def oldSchool():
+    #
+    # oPositive = []
+    # oNegative = []
+    # oNeutral = []
+    #
+    # for data in sole['What do you think about Paragon university security? (can be Vehicle parking, Loss and found etc...)   State your experience if you have once.']:
+    #     ASubject = TextBlob(data)
+    #     print(data)
+
+
+#
+# Example = ["The food was great! But I didn't like the service.",
+#            "The receptionist is friendly and very helpful toward finding information about the school for us new students",
+#            "The atmosphere is nice and the service was helpful when it comes to food, I would say 7/10.",
+#            "Not my style, I don't recommend it."
+#            ]
+#
+#
+# for phraseExample in Example:
+#     dataExample = TextBlob(Example)
+#     print("Sentence: " + str(phraseExample))
+#     print("Sentiment Score: " + str(dataExample.sentiment.polarity))
+#     print("Result: " + sentiment_result(dataExample.sentiment.polarity))
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def algo(duckyhead):
 
     df = duckyhead
@@ -212,6 +270,8 @@ def algo(duckyhead):
     neg_canteen_sent = 0
 
     newInf = pos_service_sent, pos_tuition_sent + 1, pos_secure_sent, pos_env_sent + 1
+
+
 
     for phrase in df['TextDataReview']: #df['TextDataReview']
         manjiGang = TextBlob(phrase)
@@ -412,209 +472,15 @@ def algo(duckyhead):
     }
 
 
-# negative_point = 0
-# Neutral_point = 0
-# positive_point = 0
-#
-# pos_service_sent = 0
-# neg_service_sent =0
-# neut_service_sent = 0
-#
-# pos_tuition_sent = 0
-# neg_tuition_sent = 0
-# neut_tuition_sent = 0
-#
-# pos_secure_sent = 0
-# neg_secure_sent = 0
-# neut_secure_sent = 0
-#
-# pos_env_sent = 0
-# neg_env_sent = 0
-# neut_env_sent = 0
-#
-# pos_canteen_sent = 0
-# neut_canteen_sent = 0
-# neg_canteen_sent = 0
-#
-# newInf = pos_service_sent, pos_tuition_sent+1, pos_secure_sent, pos_env_sent+1
-#
-#
-# for phrase in df['TextDataReview']:
-#     manjiGang = TextBlob(phrase)
-#     poleRate = ["1", "2", "3", "4", "5"]
-#
-#     word = 0
-#     for ratingSession in poleRate:
-#         # Supporting the Pole
-#         if phrase == ratingSession:
-#             # print("Sentiment Point: " + str(ratingCanteen(phrase)))
-#             # print("Sentence: Poll review for canteen => :" + phrase)
-#             # print("Polarity result: " + sentiment_result(ratingCanteen(phrase)))
-#             # print("\n")
-#             cant_sent += 1
-#             if sentiment_result(ratingCanteen(phrase)) == "Positive":
-#                 positive_point += 1
-#                 pos_canteen_sent += 1
-#             if sentiment_result(ratingCanteen(phrase)) == "Negative":
-#                 negative_point += 1
-#                 neg_canteen_sent += 1
-#             if sentiment_result(ratingCanteen(phrase)) == "Neutral":
-#                 Neutral_point += 1
-#                 neut_canteen_sent += 1
-#
-#
-#
-#     # Regular data
-#     if phrase != "1":
-#         if phrase != "2":
-#             if phrase != "3":
-#                 if phrase != "4":
-#                     if phrase != "5":
-#                         # print("Sentiment point:  " + str(manjiGang.sentiment.polarity))
-#                         # print("Sentence :" + phrase)
-#                         # print("Polarity result: " + sentiment_result(manjiGang.sentiment.polarity))
-#                         # print("\n")
-#                         if sentiment_result(manjiGang.sentiment.polarity) == "Positive":
-#                             positive_point += 1
-#                             # pos neg neu of each cate guidance
-#                             if isService(phrase) == True:
-#                                 pos_service_sent += 1
-#
-#                             elif isTuition(phrase) == True:
-#                                 pos_tuition_sent += 1
-#
-#                             elif isSecurity(phrase) == True:
-#                                 pos_secure_sent += 1
-#
-#                             elif isEnvironment(phrase) == True:
-#                                 pos_env_sent += 1
-#
-#                         if sentiment_result(manjiGang.sentiment.polarity) == "Negative":
-#                             negative_point += 1
-#                             if isService(phrase) == True:
-#                                 neg_service_sent += 1
-#
-#                             elif isTuition(phrase) == True:
-#                                 neg_tuition_sent += 1
-#
-#                             elif isSecurity(phrase) == True:
-#                                 neg_secure_sent += 1
-#
-#                             elif isEnvironment(phrase) == True:
-#                                 neg_env_sent += 1
-#
-#                         if sentiment_result(manjiGang.sentiment.polarity) == "Neutral":
-#                             Neutral_point += 1
-#                             if isService(phrase) == True:
-#                                 neut_service_sent += 1
-#                             elif isTuition(phrase) == True:
-#                                 neut_tuition_sent += 1
-#                             elif isSecurity(phrase) == True:
-#                                 neut_secure_sent += 1
-#                             elif isEnvironment(phrase) == True:
-#                                 neut_env_sent += 1
-#
-#                         if isService(phrase) == True:
-#                             service_sent += 1
-#
-#                         if isTuition(phrase) == True:
-#                             tuition_sent += 1
-#
-#                         if isSecurity(phrase) == True:
-#                             secure_sent += 1
-#
-#                         if isEnvironment(phrase) == True:
-#                             env_sent += 1
-#
-# newEgg =[]
-# print("Service ========> " + str(service_sent))
-# print("tuition ========> " + str(tuition_sent))
-# print("Security ========> " + str(secure_sent))
-# print("Environment ========> " + str(env_sent))
-# print("canteen ========> " + str(cant_sent))
-#
-# # serviceFullInfo()
-# # tuitionFullInfo()
-# # envFullInfo()
-# # securityFullInfo()
-# pos_category= []
-#
-# pos_category.append(pos_secure_sent)
-# pos_category.append(pos_service_sent)
-# pos_category.append(pos_env_sent+1)
-# pos_category.append(pos_tuition_sent+1)
-# pos_category.append(pos_canteen_sent)
-#
-# neg_category = []
-#
-# neg_category.append(neg_secure_sent)
-# neg_category.append(neg_service_sent)
-# neg_category.append(neg_env_sent)
-# neg_category.append(neg_tuition_sent)
-# neg_category.append(neg_canteen_sent)
-#
-#
-# neut_category = []
-#
-# neut_category.append(neut_secure_sent)
-# neut_category.append(neut_service_sent)
-# neut_category.append(neut_env_sent)
-# neut_category.append(neut_tuition_sent)
-# neut_category.append(neut_canteen_sent)
-#
-#
-#
-#
-#
-# total_sen = 0
-#
-# for something in df['TextDataReview']:
-#     total_sen+=1
-#
-# print("Total Data Sentiment: " + str(total_sen))
-
-# for phrase in df['TextDataReview']:
-#     yeet = TextBlob(phrase)
-#     if isSecurity(phrase) == True:
-#         print(phrase + ' = '+ sentiment_result(yeet.sentiment.polarity) )
-
-
-
-
-# for word in all_words:
-#     if word not in superior:
-#         print(word)
-
-
-# print(all_words[9:10])
-
-#
-# Example = ["Toilet smells bad.",
-#            "The receptionist is friendly and very helpful toward finding information about the school for us new students.",
-#
-#            "Not my style I don't recommend it"]
-#
-#
-# for phraseExample in Example:
-#     dataExample = TextBlob(phraseExample)
-#
-#     print("Sentence: " + str(dataExample) )
-#     print("Sentiment Score: " + str(dataExample.sentiment.polarity))
-#     print("Result: " + sentiment_result(dataExample.sentiment.polarity))
-#     print("")
 
 
 
 
 
-
-
-@app.route("/",methods = ["GET"])
+@app.route("/sample",methods = ["GET", "POST"])
 def hello():
 
     dataAl=algo(None)
-
-
 
 
     return render_template('index.html', total_sen=dataAl['total_sen'],
@@ -623,26 +489,39 @@ def hello():
                            positive_point=dataAl['positive_point'], negative_point=dataAl['negative_point'], Neutral_point=dataAl['Neutral_point'],
                            fwk=dataAl['fwk'], fwv=dataAl['fwv'])
 
-@app.route('/howItWork',methods = ["GET"])
+
+
+
+
+
+@app.route('/',methods = ["GET", "POST"])
 def howItWork():
     return render_template('spart.html')
 
 @app.route("/uploadingPage", methods=['POST'])
 
 def uploadingPage():
-    if 'csv' in request.files:
-        csvFile = request.files['csv']
-        duckyHead = pd.read_csv(csvFile)
-        dataAl = algo(duckyHead)
-        print(dataAl)
 
-        return render_template('index.html', total_sen=dataAl['total_sen'],
-                               neg_p=dataAl['negative_point'], neut_p=dataAl['Neutral_point'],
-                               pos_category=dataAl['pos_category'],
-                               neg_category=dataAl['neg_category'], neut_category=dataAl['neut_category'],
-                               positive_point=dataAl['positive_point'], negative_point=dataAl['negative_point'],
-                               Neutral_point=dataAl['Neutral_point'],
-                               fwk=dataAl['fwk'], fwv=dataAl['fwv'])
+
+    
+    if 'filecsv' in request.files:
+        csvFile = request.files['filecsv']
+        print(csvFile)
+        duckyHead = pd.read_csv(csvFile)
+        if "TextDataReview" in duckyHead.head().to_string():
+            dataAl = algo(duckyHead)
+            print(dataAl)
+
+            return render_template('index.html', total_sen=dataAl['total_sen'],
+                                   neg_p=dataAl['negative_point'], neut_p=dataAl['Neutral_point'],
+                                   pos_category=dataAl['pos_category'],
+                                   neg_category=dataAl['neg_category'], neut_category=dataAl['neut_category'],
+                                   positive_point=dataAl['positive_point'], negative_point=dataAl['negative_point'],
+                                   Neutral_point=dataAl['Neutral_point'],
+                                   fwk=dataAl['fwk'], fwv=dataAl['fwv'])
+
+        else:
+            return render_template('error-handles.html')
 
 @app.route("/tested", methods= ["GET"])
 def testingy():
@@ -666,50 +545,3 @@ if __name__ == '__main__':
 
 
 
-        # "if k == "True":
-        #     print("Non-English word detected in the sentence")
-        # else:
-        #     print(create_word_features(testimonial.words))
-
-
-
-
-# someA = TextBlob("room is not dirty but omygood")
-# print(balancingScore(someA.sentiment.polarity, someA))
-#
-# print(sentiment_result(someA.sentiment.polarity))
-
-
-
-
-
-
-# training = [
-# ('I think there are a lot food court or restaurant nearby that just have much better menu than our school canteen.','negative'),
-# ('It is not so great to fit in a small room without air','negative'),
-# ('The Dark Knight Rises is the greatest superhero movie ever!','positive'),
-# ('Fantastic Four should have never been made.','positive'),
-# ('Wes Anderson is my favorite director!','positive'),
-# ('Captain America 2 is pretty awesome.','positive'),
-# ('The security guard did not know what they are doing despite taking a huge amount of salary ','negative'),
-# ]
-# testing = [
-# ('Superman was never an interesting character.','pos'),
-# ('Fantastic Mr Fox is an awesome film!','neg'),
-# ('Dragonball Evolution is simply terrible!!','pos')
-# ]
-# classifier = classifiers.NaiveBayesClassifier(training)
-# print (classifier.accuracy(testing))
-#
-#
-# blob = TextBlob('the guard did not pay attention to anything beside sitting down and play phone', classifier=classifier)
-# print (classifier.accuracy(blob))
-
-
-# Money = TextBlob("")
-# print(sentiment_result(Money.sentiment.polarity))
-# print(Money.sentiment.polarity)
-
-
-
-# print(" We got our polarity with result: " + str(testimonial.sentiment.polarity))
